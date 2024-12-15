4be764629c75b327d7dd8cecfe684c6e854a5953
JsExpertCoder <misterjs247000@gmail.com>
2024-12-15 18:53:12 +0100

    add markets marker on map

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react"
-import { View, Alert } from "react-native"
-
-import * as Location from "expo-location"
+import { View, Alert, Text } from "react-native"
 import MapView, { Callout, Marker } from "react-native-maps"
+import * as Location from "expo-location"
+import { router } from "expo-router"
 
 import { api } from "@/services/api"
+import { fontFamily, colors } from "@/styles/theme"
 
 import { Places } from "@/components/places"
 import { PlaceProps } from "@/components/place"
@@ -98,6 +99,42 @@ export default function Home() {
                     }}
                     image={require("@/assets/location.png")}
                 />
+
+                {markets.map((item) => (
+                    <Marker
+                        key={item.id}
+                        identifier={item.id}
+                        coordinate={{
+                            latitude: item.latitude,
+                            longitude: item.longitude,
+                        }}
+                        image={require("@/assets/pin.png")}
+                    >
+                        <Callout onPress={() => router.navigate(`/market/${item.id}`)}>
+                            <View>
+                                <Text
+                                    style={{
+                                        fontSize: 14,
+                                        color: colors.gray[600],
+                                        fontFamily: fontFamily.medium,
+                                    }}
+                                >
+                                    {item.name}
+                                </Text>
+
+                                <Text
+                                    style={{
+                                        fontSize: 12,
+                                        color: colors.gray[600],
+                                        fontFamily: fontFamily.regular,
+                                    }}
+                                >
+                                    {item.address}
+                                </Text>
+                            </View>
+                        </Callout>
+                    </Marker>
+                ))}
             </MapView>
             <Places data={markets} />
         </View>
