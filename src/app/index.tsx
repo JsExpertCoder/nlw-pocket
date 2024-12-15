392e3d232c3d381d422e081d91615cc597ec8b44
JsExpertCoder <misterjs247000@gmail.com>
2024-12-15 14:17:53 +0100

    remove button icon

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,18 @@
 import { View } from 'react-native'
 
-import { Welcome } from '@/components/welcome'
 import { Steps } from '@/components/steps'
+import { Button } from '@/components/button'
+import { Welcome } from '@/components/welcome'
+import { IconCheck } from '@tabler/icons-react-native'
 
 export default function Index() {
   return (
-    <View style={{flex: 1, padding: 40, gap: 40}}>
+    <View style={{ flex: 1, padding: 40, gap: 40 }}>
       <Welcome />
       <Steps />
+      <Button>
+        <Button.Title>Começar</Button.Title>
+      </Button>
     </View>
   )
 }
